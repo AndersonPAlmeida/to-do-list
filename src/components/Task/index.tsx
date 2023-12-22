2bdbca0c92c866a258a6702160d19ce00707cdbf
Anderson Almeida <andersonpa10pa@gmail.com>
2023-12-21 22:35:05 -0300

    Ajustes de estilo nas Tasks

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,13 +4,17 @@ import { CgTrash } from "react-icons/cg";
 export function Task() {
    return (
       <div className={style.container}>
-         <input type="checkbox" name="completionTask" id="completionTask" />
+         <span className={style.containerCheck}>
+            <input type="checkbox" name="completionTask" id="completionTask" />
+         </span>
          <p className={style.descriptionTask}>
             Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
          </p>
-         <button>
-            <CgTrash size={22}/>
-         </button>
+         <span className={style.containerButton}>
+            <button>
+               <CgTrash size={18}/>
+            </button>
+         </span>
       </div>
    );
 }
