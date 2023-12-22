5d8cac9dad6a20646662aaa713ee9e36259aeacb
Anderson Almeida <andersonpa10pa@gmail.com>
2023-12-21 21:19:29 -0300

    Ajustes nos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Header } from './components/Header';
 import { AddTask } from './components/AddTask';
+import { Tasks } from './components/ListTasks';
 
 import './global.css';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
       <Header />
-      <AddTask />
+      <div className={styles.container}>
+        <AddTask />
+        <Tasks />
+      </div>
     </>
   )
 }
